93ddfefff82ff60fd91506f62dde5df6824e5506
guleDev <gule.dev64@gmail.com>
2024-06-15 16:37:32 -0300

    feat: Animated Counter amount

--- src/components/TotalBalanceBox.tsx
+++ src/components/TotalBalanceBox.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import AnimatedCounter from "./AnimatedCounter";
 
-const TotalBalanceBox = () => {
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* DoughnutChart */}</div>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">{totalBanks} Bank Accounts</h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balace</p>
+          <p className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance}/>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default TotalBalanceBox
+export default TotalBalanceBox;
