18fa473223ad40ed753370a92b39dc58cc3ffdc6
guleDev <gule.dev64@gmail.com>
2024-06-20 12:33:32 -0300

    feat: desktop sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,26 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Gustavo", lastName: "Pampu" };
+
   return (
-    <main>
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex sixe-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
